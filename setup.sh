356038355e186fcd9d97ff46903dfcf08be970c9
tristanescalada <tristan@escalada.us>
2021-07-19 11:35:56 -0400

    Switching keyservers
    The sks keyserver is deprecated.
    Switched to keyserver.ubuntu.com and that seems to have fixed the issue.

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ echo deb 'http://cz.archive.ubuntu.com/ubuntu bionic main universe' >> /etc/apt/
 
 ## Add repository PUB_KEY
 KEYS=$(apt update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p')
-echo $KEYS | while read key; do apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys "$key"; done
+echo $KEYS | while read key; do apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$key"; done
